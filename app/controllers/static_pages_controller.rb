4dc05523f73b84b80daa8c1743789b99f8b3eb5e
Junior Souz4 <juniorsouz4ads@gmail.com>
2018-12-16 10:23:02 -0300

    aplicando partials ao projeto

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,13 @@ class StaticPagesController < ApplicationController
     def index
         @name = "Souza Junior"
     end
+
+    def contato
+        
+    end
+
+    def sobre
+
+    end
     
 end
